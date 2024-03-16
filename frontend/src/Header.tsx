c855c7511d464024c8630f65b70f5fe648314f43
maurineiko <nohaskett@gmail.com>
2024-03-15 22:55:36 -0600

    prettier stuff and header

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <div className="subtitle">Bowlers Galore</div>
+      <p>
+        Welcome to the bowlers site. I will include a better description later.
+      </p>
+    </header>
+  );
+}
+
+export default Header;
